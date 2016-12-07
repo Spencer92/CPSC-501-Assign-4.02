aec93f8f502f72f59e7db3800c88701769e1d2ab
Spencer Maslen <smhmaslen@gmail.com>
2016-12-07 15:45:51 -0700

    Staring to add convolve

---
+++ CPSC 501 Assign 4/convolve.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC_501_Assign_4
+{
+    class convolve
+    {
+        public void convolveSln(float x[], int N, float h[], int M, float y[], int P)
+        {
+            int n, m; /*  Make sure the output buffer is the right size: P = N + M - 1  */
+            if(P != (N+M-1))
+            {
+                Console.Write
+            }
+        }
+
+    }
+}
