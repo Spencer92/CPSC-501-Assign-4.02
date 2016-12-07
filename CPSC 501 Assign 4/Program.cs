1bc9daa7cccf8ed85179206bc6723b3ade1ef125
Spencer Maslen <Spencer Maslen@DESKTOP-8IPIKTH>
2016-12-07 12:52:11 -0700

    Add project files.

---
+++ CPSC 501 Assign 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CPSC_501_Assign_4
+{
+
+    class Program
+    {
+        static int BYTE = 16;
+        
+
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("Flute.wav");
+            int mainSoundLength = 0;
+            int environmentLength = 0;
+
+
+
+        }
+    }
+}
