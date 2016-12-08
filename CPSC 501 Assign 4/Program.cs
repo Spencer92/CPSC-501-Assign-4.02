4c5aa8c001690f1d3974643670804dd3643d61c4
Spencer <smhmaslen@gmail.com>
2016-12-07 19:13:47 -0700

    Figuring things out.

--- CPSC 501 Assign 4/Program.cs
+++ CPSC 501 Assign 4/Program.cs
@@ -11,16 +11,164 @@ namespace CPSC_501_Assign_4
     class Program
     {
         static int BYTE = 16;
-        
+        static ushort CHAR_SIZE = 256;
+        static uint NEGATIVE_FLOAT = 32768;
+        static uint POSITIVE_FLOAT = 32767;
+
 
         static void Main(string[] args)
         {
-            StreamReader streamReader = new StreamReader("Flute.wav");
-            int mainSoundLength = 0;
+            StreamReader audioData = new StreamReader("FluteDry.wav");
+            StreamWriter output;
+            int soundLength = 0;
             int environmentLength = 0;
             int stuff;
+            float[] newDryValues;
+            float[] areaData;
+            float[] convolvedData;
+            short[] convertedData;
+            ushort digit;
+            float shortDigit;
+            int index = 0;
+            int length;
+            float thing;
+            Convolve convolver;
+            float largest_value = 1;
+
+            while (!audioData.EndOfStream)
+            {
+                audioData.Read();
+                soundLength++;
+            }
+
+            audioData.Close();
+            audioData = new StreamReader("FluteDry.wav");
+
+            newDryValues = new float[(soundLength / 2) + 1];
+            length = (soundLength / 2) + 1;
+
+            while (!audioData.EndOfStream)
+            {
+                digit = Convert.ToUInt16(audioData.Read());
+                digit *= CHAR_SIZE;
+                if (!audioData.EndOfStream)
+                {
+                    digit += Convert.ToUInt16(audioData.Read());
+                }
+
+                shortDigit = (float)digit;
+
+                if (index < length)
+                {
+                    newDryValues[index] = shortDigit / NEGATIVE_FLOAT;
+                    Console.WriteLine(newDryValues[index]);
+                    index++;
+                }
+            }
+//            Console.ReadLine();
 
 
+            audioData.Close();
+            audioData = new StreamReader("BIG HALL E001 M2S.wav");
+
+            for (int i = 0; i < newDryValues.Length; i++)
+            {
+                Console.WriteLine(newDryValues[i]);
+            }
+
+            while(!audioData.EndOfStream)
+            {
+                audioData.Read();
+                environmentLength++;
+            }
+            audioData.Close();
+
+            audioData = new StreamReader("BIG HALL E001 M2S.wav");
+            areaData = new float[(environmentLength / 2) + 1];
+            length = environmentLength / 2 + 1;
+
+            index = 0;
+            while (!audioData.EndOfStream)
+            {
+                digit = Convert.ToUInt16(audioData.Read());
+                digit *= CHAR_SIZE;
+                if (!audioData.EndOfStream)
+                {
+                    digit += Convert.ToUInt16(audioData.Read());
+                }
+
+                shortDigit = (float)digit;
+
+                if (index < length)
+                {
+                    thing = shortDigit / NEGATIVE_FLOAT;
+                    areaData[index] = shortDigit / NEGATIVE_FLOAT;
+                    Console.Write(areaData[index]);
+                    index++;
+                }
+            }
+            audioData.Close();
+
+            convolvedData = new float[areaData.Length + newDryValues.Length - 1];
+            Console.WriteLine("Right before convolve");
+
+            /*            for(int i = 0; i < newDryValues.Length; i++)
+                        {
+                            Console.Write(newDryValues[i]);
+                        }*/
+
+//            Console.ReadLine();
+            for(int i = 0; i < areaData.Length; i++)
+            {
+                Console.WriteLine(areaData[i]);
+            }
+
+            convolver = new Convolve();
+            convolver.convolveSln(newDryValues, newDryValues.Length, areaData, areaData.Length, convolvedData, convolvedData.Length);
+
+            convertedData = new short[convolvedData.Length];
+            length = convertedData.Length;
+
+            output = new StreamWriter("output.wav");
+
+
+            for(int i = 0; i < convolvedData.Length; i++)
+            {
+                if(convolvedData[i] > largest_value)
+                {
+                    Console.WriteLine("Too big: ");
+                    largest_value = convolvedData[i];
+                }
+            }
+
+            for(int i = 0; i < convolvedData.Length; i++)
+            {
+                convolvedData[i] /= largest_value;
+            }
+
+            for(int i = 0; i < convertedData.Length; i++)
+            {
+                convertedData[i] = Convert.ToInt16(convolvedData[i] * POSITIVE_FLOAT);
+            }
+            char value;
+            byte byteValue;
+            for(int i = 0; i < length; i++)
+            {
+                byteValue = Convert.ToByte(convertedData[i] / CHAR_SIZE);
+                value = (char) byteValue;
+                Console.WriteLine(value);
+                output.Write(value);
+                byteValue = Convert.ToByte(convertedData[i]%256);
+                value = (char)byteValue;
+                Console.WriteLine(value);
+                output.Write(value);
+
+            }
+
+            Console.Write("End of program");
+            output.Close();
+            Console.Read();
+      
 
         }
     }
--- CPSC 501 Assign 4/convolve.cs
+++ CPSC 501 Assign 4/convolve.cs
@@ -6,15 +6,67 @@ using System.Threading.Tasks;
 
 namespace CPSC_501_Assign_4
 {
-    class convolve
+    class Convolve
     {
-        public void convolveSln(float x[], int N, float h[], int M, float y[], int P)
+        public void convolveSln(float[] x, int N, float[] h, int M, float[] y, int P)
         {
-            int n, m; /*  Make sure the output buffer is the right size: P = N + M - 1  */
+            int n; /*  Make sure the output buffer is the right size: P = N + M - 1  */
+            int m = 0;
             if(P != (N+M-1))
             {
-                Console.Write
+                Console.WriteLine("Output signal vector is the wrong size");
+                Console.WriteLine("It is " + P + " but should be " + (N + M - 1));
+                Console.WriteLine("Aborting Convolution");
+                return;
             }
+            //Clear the output buffer y[] to all zero values
+            for(n = 0; n < P; n++)
+            {
+                y[n] = 0.0F; //Do the convoltion;
+            }
+            Console.ReadLine();
+            for(int i = 0; i < x.Length; i++)
+            {
+                Console.Write(x[i]);
+            }
+
+            Console.ReadLine();
+            for(int i = 0; i < h.Length; i++)
+            {
+                Console.Write(h[i]);
+            }
+
+
+            //Outer loop: process each input value x[n] in turn
+            Console.WriteLine("Length of y: " + y.Length);
+            Console.WriteLine("Length of n + m" + (n + m));
+//           float stuff = h[0];
+//            stuff = x[0];
+
+//            try
+//            {
+                for (n = 0; n < N; n++)
+                {
+                //Inner loop: process x[n] with each sample of h[]
+                for (m = 0; m < M; m++)
+                {
+                        y[n + m]
+                            += x[n]
+                            * h[m];
+                    }
+                }
+//            }
+/*            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("y is " + y.Length);
+                Console.WriteLine("n + m is " + (m + n));
+                Console.WriteLine("x is " + x.Length);
+                Console.WriteLine("n is " + n);
+                Console.WriteLine("h is " + h.Length);
+                Console.WriteLine("m is " + m);
+                Console.WriteLine("P is " + P);
+                Console.Read();
+            }*/
         }
 
     }
